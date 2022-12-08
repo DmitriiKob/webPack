3c9f866be3e06b1bc26ca41bc541c01803bd4f94
DmitriiKob <dmitriykob1002@gmail.com>
2022-12-08 10:58:59 -0800

    lesson2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+import dragon from './dragon.svg'
+class WorldName {
+    #name
+
+    constructor() {
+        this.#name = ''
+    }
+
+    setName() {
+        this.#name = prompt("Please write your name")
+    }
+
+    getName() {
+        return this.#name
+    }
+}
+
+const myName = new WorldName()
+myName.setName()
+
+const heading = document.createElement('h1')
+heading.textContent = `Hello world of ${myName.getName()}!`
+
+const root = document.querySelector('#root')
+root.append(heading)
+const img = document.createElement('img')
+img.src = dragon
+
+root.append(img)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
@@ -8,4 +8,32 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack template',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/inline',
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.ico$/],
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  }
 }
